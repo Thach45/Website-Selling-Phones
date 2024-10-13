876e7990e4ea933073169e97a774a5cbfa53b64c
hoangthach.dev <nguyenhoangthach2005@gmail.com>
2024-10-13 14:14:35 +0700

    add login

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,7 +1,7 @@
 const User = require("../../model/user.model");
 const md5 = require("md5");
 module.exports.register = async (req, res) => {
-    
+
     res.render("client/pages/user/register.pug", {
         pageTitle: "Đăng ký tài khoản"
     })
@@ -9,8 +9,8 @@ module.exports.register = async (req, res) => {
 
 module.exports.registerPost = async (req, res) => {
     req.body.password = md5(req.body.password);
-    const exitEmail = await User.findOne({email: req.body.email});
-    if(exitEmail){
+    const exitEmail = await User.findOne({ email: req.body.email });
+    if (exitEmail) {
         res.render("client/pages/user/register.pug", {
             pageTitle: "Đăng ký tài khoản",
             error: "Email đã tồn tại"
@@ -20,7 +20,44 @@ module.exports.registerPost = async (req, res) => {
     console.log(req.body)
     const user = new User(req.body);
     await user.save();
-    res.cookie("tokenUser",user.tokenUser)
+    res.cookie("tokenUser", user.tokenUser)
     res.redirect("/");
-}   
+}
+
+module.exports.login = async (req, res) => {
+    res.render("client/pages/user/login.pug", {
+        pageTitle: "Đăng nhập"
+    })
+}
+
+module.exports.loginPost = async (req, res) => {
+    try {
+        req.body.password = md5(req.body.password);
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            res.render("client/pages/user/login.pug", {
+                error: "Email hoặc mật khẩu không đúng"
+            })
+            res.redirect("back");
+            return;
+        }
+        else if (user.password !== req.body.password) {
+            res.render("client/pages/user/login.pug", {
+                error: "Email hoặc mật khẩu không đúng"
+            })
+            res.redirect("back");
+            return;
+
+        }
+        else {
+            res.cookie("tokenUser", user.tokenUser)
+            res.redirect("/");
+        }
+
+    }
+    catch (error) {
+        res.render("client/error/index.pug", {
+        })
+    }
+}
 
